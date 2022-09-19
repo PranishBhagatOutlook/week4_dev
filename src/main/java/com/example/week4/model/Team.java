ffc4d472d6528996a47abe9b679849a983cd676e
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-09-19 16:06:20 -0500

    promoted team id

--- src/main/java/com/example/week4/model/Team.java
+++ src/main/java/com/example/week4/model/Team.java
@@ -1,9 +1,5 @@
 package com.example.week4.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-//import javax.validation.constraints.Size;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -15,18 +11,31 @@ public class Team implements Serializable {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = true)
+    private Long promotedTeamId;
     @Column(nullable = false)
     private String name;
-
     @Column(nullable = true)
     private Integer rank;
-
     @Column(nullable = false)
     private State state;
-
     //    @Size(min = 1, max =3)
     @OneToMany
     private Set<Person> contestant = new HashSet();
+    //    @Column(nullable = false)
+    @ManyToOne
+    private Contest contest;
+    //    @Size(min = 0, max =1)
+    @OneToOne
+    private Person coach;
+
+    public Long getPromotedTeamId() {
+        return promotedTeamId;
+    }
+
+    public void setPromotedTeamId(Long promotedTeamId) {
+        this.promotedTeamId = promotedTeamId;
+    }
 
     public Set<Person> getContestant() {
         return contestant;
@@ -44,11 +53,6 @@ public class Team implements Serializable {
         this.coach = coach;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
     public Contest getContest() {
         return contest;
     }
@@ -57,20 +61,12 @@ public class Team implements Serializable {
         this.contest = contest;
     }
 
-    //    @Column(nullable = false)
-    @ManyToOne
-    private Contest contest;
-
-    //    @Size(min = 0, max =1)
-    @OneToOne
-    private Person coach;
-
-    public static enum State {
-        ACCEPTED, PENDING, CANCELED
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -95,6 +91,10 @@ public class Team implements Serializable {
 
     public void setState(State state) {
         this.state = state;
+    }
+
+    public static enum State {
+        ACCEPTED, PENDING, CANCELED
     }
 
 
